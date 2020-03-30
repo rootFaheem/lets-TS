d7285b2ee1d89733ac4c7026db4de122a35567b8
rootFaheem <faheemtech1@gmail.com>
2020-03-30 15:30:00 +0530

    tyopes listed, including new types

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 "use strict";
-//  TYPES
-var myName = "elon";
-var myShoeSize = 8;
-var isGoodDeveloper = true;
-//  what we can't do
-myName = 2;
-myShoeSize = "Eight";
-isGoodDeveloper = 0;
+// 1. Basic TYPES
+var myName = "elon"; // similar to => let myName: string = "elon"
+var myShoeSize = 8; // similar to => let myShoeSize: number = 8
+var isGoodDeveloper = true; // similar to => let myName: boolean = true
+//  what we can't do to above assigned values.
+// myName = 2;
+// myShoeSize = "Eight";
+// isGoodDeveloper = 0;
+// 2. Explicitly assigning
+var myRealAge;
+myRealAge = 22;
+// myRealAge = "Twenty Two";
+// 3. Array
+var hobbies = ["development", "DevOps"];
+hobbies[0] = 100;
+console.log(typeof hobbies[0]);
+// NEW TYPES ADDED WITH TYPESCRIPT
+// 4. Tuples (an array with limited items each having a specific type and will remain the same type)
+var myAddress = ["Silicon Valley", 22];
+// 5. Enum
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 20] = "Green";
+    Color[Color["Blue"] = 21] = "Blue"; // 21
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(myColor);
+// 6. any
+var myFavouriteCar = "Tesla";
+console.log("before:", myFavouriteCar);
+myFavouriteCar = {
+    name: "Tesla",
+    model: "Y"
+};
+console.log("after:", myFavouriteCar);
--- app.ts
+++ app.ts
@@ -1,11 +1,44 @@
-//  TYPES
+// 1. Basic TYPES
+let myName = "elon"; // similar to => let myName: string = "elon"
+let myShoeSize = 8; // similar to => let myShoeSize: number = 8
+let isGoodDeveloper = true; // similar to => let myName: boolean = true
 
-let myName = "elon";
-let myShoeSize = 8;
-let isGoodDeveloper = true;
-
-//  what we can't do
-
+//  what we can't do to above assigned values.
 // myName = 2;
 // myShoeSize = "Eight";
 // isGoodDeveloper = 0;
+
+// 2. Explicitly assigning
+let myRealAge: number;
+myRealAge = 22;
+// myRealAge = "Twenty Two";
+
+// 3. Array
+let hobbies: any[] = ["development", "DevOps"];
+hobbies[0] = 100;
+console.log(typeof hobbies[0]);
+
+// NEW TYPES ADDED WITH TYPESCRIPT
+
+// 4. Tuples (an array with limited items each having a specific type and will remain the same type)
+let myAddress: [string, number] = ["Silicon Valley", 22];
+
+// 5. Enum
+enum Color {
+  Gray, // 0
+  Green = 20,
+  Blue // 21
+}
+let myColor: Color = Color.Green;
+console.log(myColor);
+
+// 6. any
+let myFavouriteCar: any = "Tesla";
+console.log("before:", myFavouriteCar);
+
+myFavouriteCar = {
+  name: "Tesla",
+  model: "Y"
+};
+
+console.log("after:", myFavouriteCar);
