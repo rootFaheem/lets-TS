4a7479d709d3ef94b15e76a93edf1bd9d8daa321
rootFaheem <faheemtech1@gmail.com>
2020-03-31 23:16:04 +0530

    phase 1 -> solved

--- exercises/1.ts
+++ exercises/1.ts
@@ -1,17 +1,29 @@
 // Q. Convert this js code to TS
-let banAccount = {
+// let banAccount = {
+//     money: 2000,
+//     deposit(value) {
+//         this.money += value;
+//     }
+// };
+// let myself = {
+//     name: "Elon",
+//     bankAccount: bankAccount,
+//     hobbies: ["Coding", "Space"]
+// };
+// myself.bankAccount.deposit(30000);
+// console.log(myselft);
+
+// Solution
+let bankAccount: { money: number; deposit: (val: number) => void } = {
     money: 2000,
-    deposit(value) {
+    deposit(value: number) {
         this.money += value;
     }
 };
-
 let myself = {
     name: "Elon",
     bankAccount: bankAccount,
-    hobbies: ["Coding", "Space"]
+    hobbies: ["coding", "Space"]
 };
-
-myself.bankAccount.deposit(30000);
-
-console.log(myselft);
+myself.bankAccount.deposit(3000);
+console.log(myself);
