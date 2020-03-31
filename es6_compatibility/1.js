e614da365c13eeb9cced41f7dde6d37e38730d89
rootFaheem <faheemtech1@gmail.com>
2020-04-01 00:41:53 +0530

    arrow function and variations

--- es6_compatibility/1.js
+++ es6_compatibility/1.js
@@ -19,3 +19,22 @@ function reset() {
 }
 reset();
 console.log("outside reset(): ", var1);
+// ****** Arrow Functions ******
+console.log("ARROW FUNCTION");
+// Normal way of writing function
+var addNum = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(addNum(20, 40));
+// Arrow Function
+var multiplyNum = function (num1, num2) { return num1 * num2; };
+console.log(multiplyNum(2, 4));
+// variations
+var greet = function () {
+    console.log("Hello!");
+};
+greet();
+var greetFriend = function (friend) {
+    console.log("Hello", friend);
+};
+greetFriend("Musk");
--- es6_compatibility/1.ts
+++ es6_compatibility/1.ts
@@ -21,3 +21,26 @@ function reset() {
 }
 reset();
 console.log("outside reset(): ", var1);
+
+// ****** Arrow Functions ******
+console.log("ARROW FUNCTION");
+// Normal way of writing function
+const addNum = function(num1: number, num2: number): number {
+    return num1 + num2;
+};
+console.log(addNum(20, 40));
+// Arrow Function
+const multiplyNum = (num1: number, num2: number) => num1 * num2;
+console.log(multiplyNum(2, 4));
+
+// variations
+const greet = (): void => {
+    console.log("Hello!");
+};
+greet();
+
+const greetFriend = (friend: string): void => {
+    console.log("Hello", friend);
+};
+
+greetFriend("Musk");
